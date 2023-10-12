199eac58e6c366beee5f2de3f278b1a09618a5a2
Frankhjy <55027597+Frankhjy@users.noreply.github.com>
2023-10-12 17:04:38 +0800

    Add files via upload

--- SQL学习.sql
+++ SQL学习.sql
@@ -1,3 +1,4 @@
+-- Active: 1695730306959@@127.0.0.1@3308@db_school
 #创建数据库 
  CREATE DATABASE db_school
  DEFAULT CHARACTER SET GB2312
@@ -42,6 +43,63 @@
     classNo CHAR(6)
  ) ENGINE=InnoDB;
 
+ DROP TABLE tb_student;
+ CREATE TABLE db_school.tb_student
+ (
+    studentNo CHAR(10),
+    studentName VARCHAR(20)NOT NULL,
+    sex CHAR(2) NOT NULL,
+    birthday DATE,
+    native VARCHAR(20),
+    nation VARCHAR(10) DEFAULT'汉',
+    classNo CHAR(6),
+    CONSTRAINT PK_student PRIMARY KEY(studentNo)
+ ) ENGINE=InnoDB;
 
- 
+CREATE TABLE tb_class
+(
+   classNo CHAR(6) PRIMARY KEY,
+   className VARCHAR(20) NOT NULL  UNIQUE,
+   department VARCHAR(30) NOT NULL,
+   grade SMALLINT,
+   classNum TINYINT
+)ENGINE=InnoDB;
+
+DROP TABLE tb_class;
+
+CREATE TABLE tb_class
+(
+   classNo CHAR(6) PRIMARY KEY,
+   className VARCHAR(20) NOT NULL,
+   department VARCHAR(30) NOT NULL,
+   grade SMALLINT,
+   classNum TINYINT,
+   CONSTRAINT UQ_class UNIQUE(className)
+)ENGINE=InnoDB;
+
+CREATE TABLE tb_course
+(
+   courseNo CHAR(6),
+   courseName VARCHAR(20) NOT NULL,
+   credit INT NOT NULL,
+   courseHour INT NOT NULL,
+   term CHAR(2),
+   priorCourse CHAR(6),
+   CONSTRAINT PK_course PRIMARY KEY(courseNo),
+   CONSTRAINT FK_course FOREIGN KEY(priorCourse)
+   REFERENCES tb_course(courseNo),
+   CONSTRAINT CK_course CHECK(credit=courseHour/16)
+)ENGINE=InnoDB;
+
+CREATE TABLE tb_score
+(
+   studentNo CHAR(10),
+   courseNo CHAR(6),
+   score FLOAT CHECK(score>=0 AND score<=100),
+   CONSTRAINT PK_score PRIMARY KEY(studentNo,courseNo),
+   CONSTRAINT FK_score1 FOREIGN KEY(studentNo)
+   REFERENCES tb_student(studentNo),
+   CONSTRAINT FK_score2 FOREIGN KEY(courseNo)
+   REFERENCES tb_course(courseNo)
+)ENGINE=InnoDB;
  
