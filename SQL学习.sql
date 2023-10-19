f29289b75540febf6046058f1cb08da0b3c8dbe7
Frankhjy <55027597+Frankhjy@users.noreply.github.com>
2023-10-19 16:59:37 +0800

    Add files via upload

--- SQL学习.sql
+++ SQL学习.sql
@@ -1,3 +1,4 @@
+-- Active: 1695730306959@@127.0.0.1@3308@db_school
 #创建数据库 
  CREATE DATABASE db_school
  DEFAULT CHARACTER SET GB2312
@@ -42,6 +43,96 @@
     classNo CHAR(6)
  ) ENGINE=InnoDB;
 
+ DROP TABLE tb_student;
+ CREATE TABLE db_school.tb_student
+ (
+    studentNo CHAR(10),
+    studentName VARCHAR(20)NOT NULL,
+    sex CHAR(2) NOT NULL,
+    birthday DATE,
+    native VARCHAR(20),
+    nation VARCHAR(10) DEFAULT'汉',
+    classNo CHAR(6),
+    CONSTRAINT PK_student PRIMARY KEY(studentNo)
+ ) ENGINE=InnoDB;
 
- 
+CREATE TABLE tb_class
+(
+   classNo CHAR(6) PRIMARY KEY,
+   className VARCHAR(20) NOT NULL  UNIQUE,
+   department VARCHAR(30) NOT NULL,
+   grade SMALLINT,
+   classNum TINYINT
+)ENGINE=InnoDB;
+
+DROP TABLE tb_class;
+
+CREATE TABLE tb_class
+(
+   classNo CHAR(6) PRIMARY KEY,
+   className VARCHAR(20) NOT NULL,
+   department VARCHAR(30) NOT NULL,
+   grade SMALLINT,
+   classNum TINYINT,
+   CONSTRAINT UQ_class UNIQUE(className)
+)ENGINE=InnoDB;
+
+CREATE TABLE tb_course
+(
+   courseNo CHAR(6),
+   courseName VARCHAR(20) NOT NULL,
+   credit INT NOT NULL,
+   courseHour INT NOT NULL,
+   term CHAR(2),
+   priorCourse CHAR(6),
+   CONSTRAINT PK_course PRIMARY KEY(courseNo),
+   CONSTRAINT FK_course FOREIGN KEY(priorCourse)
+   REFERENCES tb_course(courseNo),
+   CONSTRAINT CK_course CHECK(credit=courseHour/16)
+)ENGINE=InnoDB;
+
+CREATE TABLE tb_score
+(
+   studentNo CHAR(10),
+   courseNo CHAR(6),
+   score FLOAT CHECK(score>=0 AND score<=100),
+   CONSTRAINT PK_score PRIMARY KEY(studentNo,courseNo),
+   CONSTRAINT FK_score1 FOREIGN KEY(studentNo)
+   REFERENCES tb_student(studentNo),
+   CONSTRAINT FK_score2 FOREIGN KEY(courseNo)
+   REFERENCES tb_course(courseNo)
+)ENGINE=InnoDB;
+
+CREATE DATABASE db_sp
+DEFAULT CHARACTER SET GB2312
+DEFAULT COLLATE GB2312_chinese_ci;
+
+CREATE TABLE S 
+(
+   SNO CHAR(5),
+   SNAME VARCHAR(20) NOT NULL UNIQUE,
+   STATUS SMALLINT,
+   CITY VARCHAR(20),
+   CONSTRAINT PK_S PRIMARY KEY (SNO),
+   CONSTRAINT CK_S CHECK(CITY != 'London' OR STATUS=20)
+)ENGINE=InnoDB;
+
+CREATE TABLE P 
+(
+   PNO CHAR(5),
+   PNAME VARCHAR(15) NOT NULL,
+   COLOR VARCHAR(10) CHECK(COLOR IN ('Red','Yellow','Green','Blue')),
+   WEIGHT INT,
+   CONSTRAINT PK_P PRIMARY KEY (PNO) 
+)ENGINE=InnoDB;
+
+CREATE TABLE SP
+(
+   SNO CHAR(5),
+   PNO CHAR(5),
+   QTY INT,
+   CONSTRAINT PK_SPJ PRIMARY KEY (SNO,PNO),
+   CONSTRAINT FK_SPJ1 FOREIGN KEY (SNO)  REFERENCES S (SNO),
+   CONSTRAINT FK_SPJ2 FOREIGN KEY (PNO)  REFERENCES P (PNO)
+)ENGINE=InnoDB;
  
